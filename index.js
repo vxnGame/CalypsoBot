d555c61ad701baa0aca54b22bc51eaecd93da192
vxnGame <vxn.game+github@gmail.com>
2021-05-17 10:46:48 +0900

    Default

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ global.__basedir = __dirname;
 // Client setup
 const intents = new Intents();
 intents.add(
-  'GUILD_PRESENCES',
-  'GUILD_MEMBERS',
-  'GUILDS',
-  'GUILD_VOICE_STATES',
-  'GUILD_MESSAGES',
-  'GUILD_MESSAGE_REACTIONS'
+	'GUILD_PRESENCES',
+	'GUILD_MEMBERS',
+	'GUILDS',
+	'GUILD_VOICE_STATES',
+	'GUILD_MESSAGES',
+	'GUILD_MESSAGE_REACTIONS',
 );
 const client = new Client(config, { ws: { intents: intents } });
 
 // Initialize client
 function init() {
-  client.loadEvents('./src/events');
-  client.loadCommands('./src/commands');
-  client.loadTopics('./data/trivia');
-  client.login(client.token);
+	client.loadEvents('./src/events');
+	client.loadCommands('./src/commands');
+	client.loadTopics('./data/trivia');
+	client.login(client.token);
 }
 
 init();
